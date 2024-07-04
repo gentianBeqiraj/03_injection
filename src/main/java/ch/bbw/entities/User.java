c130875a16ea5ae911084487690a3cbe35d7cb0b
Gentian Beqiraj <105867457+gentianBeqiraj@users.noreply.github.com>
2024-07-04 11:55:30 +0200

    update User: added additional columns

--- src/main/java/ch/bbw/entities/User.java
+++ src/main/java/ch/bbw/entities/User.java
@@ -1,8 +1,5 @@
 package ch.bbw.entities;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +16,15 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false, unique = true)
   private String username;
+
+  @Column(nullable = false)
   private String password;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String role;
 }
