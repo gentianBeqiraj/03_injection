3cb864e3ba7a74e2082b26c3b183214f0b1bf707
Gentian Beqiraj <105867457+gentianBeqiraj@users.noreply.github.com>
2024-07-04 13:22:34 +0200

    moved UserRepository.java to new directory

--- src/main/java/ch/bbw/repository/UserRepository.java
+++ src/main/java/ch/bbw/repository/UserRepository.java
@@ -1,9 +1,11 @@
-package ch.bbw.repositories;
+package ch.bbw.repository;
 
-import ch.bbw.entities.User;
+import ch.bbw.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
   User findByUsername(String username);
 }
+
